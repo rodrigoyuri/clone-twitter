7d0fdc476cb68a8b4d6a46f03d684bf81339bc1a
Rodrigo Yuri <rodrigo33996@gmail.com>
2021-01-10 13:11:55 -0300

    Instancia do phpdotenv para variaveis de ambiente

---
+++ source/dotenv.php
@@ -0,0 +1,10 @@
+<?php
+
+$repository = Dotenv\Repository\RepositoryBuilder::createWithNoAdapters()
+    ->addAdapter(Dotenv\Repository\Adapter\EnvConstAdapter::class)
+    ->addWriter(Dotenv\Repository\Adapter\PutenvAdapter::class)
+    ->immutable()
+    ->make();
+
+$dotenv = Dotenv\Dotenv::create($repository, __DIR__);
+$dotenv->load();
