a7106b6145d9900a62932ecdf56a555fb0e1f50a
Rodrigo Yuri <rodrigo33996@gmail.com>
2021-01-10 13:12:22 -0300

    Configurações iniciais do projeto

---
+++ source/Config.php
@@ -0,0 +1,20 @@
+<?php
+
+require __DIR__ . '/dotenv.php';
+
+define("BASE_URL", getenv('ROOT_URL'));
+
+define("DATA_LAYER_CONFIG", [
+    "driver" => getenv('DB_DRIVE'),
+    "host" => getenv('DB_HOST'),
+    "port" => getenv('DB_PORT'),
+    "dbname" => getenv('DB_NAME'),
+    "username" => getenv('DB_USER_NAME'),
+    "passwd" => getenv('DB_PASSWORD'),
+    "options" => [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL
+    ]
+]);
