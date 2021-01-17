a84d527ac33e3b73eb13a19c3b6f049989668354
Rodrigo Yuri <rodrigo33996@gmail.com>
2021-01-17 13:20:48 -0300

    rota / para renderizar a pagina de login

--- index.php
+++ index.php
@@ -2,4 +2,22 @@
 
 require __DIR__ . "/vendor/autoload.php";
 
-echo 'Hello World';
+require __DIR__ . "/source/Controller/UserController.php";
+
+$router = new \CoffeeCode\Router\Router(BASE_URL);
+
+$router->namespace("Source\Controller");
+
+$router->group(null);
+$router->get("/", "User:renderLogin");
+
+$router->group("ops");
+$router->get("/{errcode}", function($data) {
+    echo "<h1>Erro: {$data['errcode']}</h1>";
+});
+
+$router->dispatch();
+
+if($router->error()) {
+    $router->redirect("/ops/{$router->error()}");
+}
