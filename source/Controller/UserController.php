9cdc6bc374516a26b93f69d402942052810f6327
Rodrigo Yuri <rodrigo33996@gmail.com>
2021-01-17 13:22:38 -0300

    Controller de user com metodo contruct e renderLogin

---
+++ source/Controller/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Source\Controller;
+
+use League\Plates\Engine;
+
+class User 
+{
+    private $view;
+
+    public function __construct()
+    {
+        $this->view = new Engine(__DIR__ . "/../templates", "php");
+    }
+
+    public function renderLogin()
+    {
+        echo $this->view->render("/login/index");
+    }
+}
