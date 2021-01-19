3b5dea8e64911260e1c0fda7ccb308d0823c66a7
Rodrigo Yuri <rodrigo33996@gmail.com>
2021-01-19 16:45:52 -0300

    template base para todos os outros templates do projeto

---
+++ source/templates/base.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="<?= urlAssets("img/favicon.png") ?>">
+    <link rel="stylesheet" href="<?= urlAssets("css/main.css")?>">
+    <?=$this->section('styles') ?>
+    <title>Twitter</title>
+</head>
+<body>
+    <?=$this->section('content') ?>
+</body>
+</html>
