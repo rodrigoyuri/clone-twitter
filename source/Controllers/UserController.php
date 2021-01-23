cee2fa24372e42b71b0f16983937bdbe74d51d11
Rodrigo Yuri <rodrigo33996@gmail.com>
2021-01-22 21:03:53 -0300

    metodo para cadastrar usuário

--- source/Controllers/UserController.php
+++ source/Controllers/UserController.php
@@ -3,18 +3,32 @@
 namespace Source\Controllers;
 
 use League\Plates\Engine;
+use Source\Models\User;
 
 class UserController 
 {
     private $view;
+    private $user;
 
     public function __construct()
     {
         $this->view = new Engine(__DIR__ . "/../templates", "php");
+        $this->user = new User();
     }
 
-    public function renderLogin()
+    public function renderLogin() : void
     {
         echo $this->view->render("/login/index");
+    }
+
+    public function create($data) : void
+    {
+        $user = $this->user->createUser($data);
+     
+        if($user) {
+            echo json_encode(array("msg" => "Cadastro realizado com sucesso!"));
+        } else {
+            echo json_encode(array("msg" => "Erro ao efetuar o cadastro"));
+        }
     }
 }
