13f341b4820eafe53b894fa8bcbe0d0229e5420e
Rodrigo Yuri <rodrigo_yuri@hotmail.com>
2021-02-01 13:07:25 -0300

    Ajuste no arquivo e estilo adicionado

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <router-view />
   </div>
-  <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+:root {
+  --main-color: #039ff5;
+  --url-image: "../img/twitter.png";
+  --font-lato: 'Lato', sans-serif;
+  --dark-color: #15202b;
+  --main-color-hover:#0286ce;
+  --dark-color-hover: #222e3bb4;
 }
 
-#nav {
-  padding: 30px;
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+html, body {
+  height: 100vh;
+  width: 100vw;
 }
 </style>
